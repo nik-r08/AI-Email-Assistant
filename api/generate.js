31854d464f70ab69da02676ab47088b2dccec07c
nik-r08 <nikhilrao.pendyal@sus.edu>
2025-06-12 22:15:14 -0500

    generate.js is changed

--- api/generate.js
+++ api/generate.js
@@ -12,6 +12,9 @@ export default async function handler(req, res) {
 
   // Handle preflight OPTIONS request
   if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     return res.status(200).end();
   }
 
